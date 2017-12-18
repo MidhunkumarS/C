3d8d004ab24884326096214c165a148ecb9c8ea4
Midhunkumar.S <34641347+MidhunkumarS@users.noreply.github.com>
2017-12-18 14:16:07 +0530

    Create variable.c

---
+++ begginer/Assignment/variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+char name[200]={"sachin tendulkar"};
+char nname[100]={"Little Master"};
+int run=18426;
+float avg=44.83;
+int match=463;
+printf("%s\n%s\n%d\n%f\n%d\n",name,nname,run,avg,match);
+return 0;
+}
