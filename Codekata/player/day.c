3f05fcdbd40c23c249d70be0ede2f32396c11594
Midhunkumar.S <34641347+MidhunkumarS@users.noreply.github.com>
2018-03-07 19:59:44 +0530

    Create day.c

---
+++ Codekata/player/day.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[100];
+	gets(a);
+	if(a=="sunday"&&"saturday")
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
